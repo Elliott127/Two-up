95d4deeeb1baa669b923562d0025a5413c7bc889
Elliott <elliott.ortmann.work@gmail.com>
2022-11-15 11:40:50 +1030

    GameViewModel Implemented Successfully

--- Views/GameView.xaml.cs
+++ Views/GameView.xaml.cs
@@ -1,41 +1,15 @@
+using Game.ViewModels;
+
 namespace Game.Views;
 
 public partial class GameView: ContentPage
 {
-	private int score;
-	private int Score
-	{
-		get { return score; }
-		set { score = value; }
-	}
-	public GameView()
+
+	public GameView(GameViewModel viewModel)
 	{
 		InitializeComponent();
-		UpdateScore(0);
+		this.BindingContext= viewModel;
 	}
 
-	public void UpdateScore(int score)
-	{
-		Score = score;
-		ScoreLabel.Text = $"Score: {Score}";
-	}
 	
-	public void SetDarkMode()
-	{
-		throw  new NotImplementedException();
-	}
-
-	private void OnTailsClicked(object sender, EventArgs e)
-	{
-		throw new NotImplementedException();
-	}
-	private void OnHeadsClicked(object sender, EventArgs e)
-	{
-		throw new NotImplementedException();
-	}
-
-	private void OnTossCoinClicked(object sender, EventArgs e)
-	{
-		throw new NotImplementedException();
-	}
 }
