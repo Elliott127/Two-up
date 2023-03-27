7b14be79b2fac64535086eb7721cf514ef136a03
Elliott.Ortmann <Elliott.Ortmann@Chamonix.com.au>
2023-03-27 15:14:37 +1030

    Adding unit tests

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -1,11 +1,18 @@
+using Game.Services;
+using Game.ViewModels;
+
 namespace UnitTests
 {
     public class Tests
     {
-        [Fact]
-        public async Task Test1()
+        []
+        public void Test1(GameViewModel viewModel, IUserService userService)
         {
-            
+            viewModel = new GameViewModel(userService);
+            using var gvm = viewModel.Monitor();
+
+            viewModel.Score = 0;
+            viewModel.Score.Should().Be(0);
         }
     }
 }
